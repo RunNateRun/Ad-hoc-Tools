1d13b87ae87346200a87c978007bd56542a10cf2
nate <nathankrunaldue@gmail.com>
2019-09-26 09:50:03 -0400

    Removed modifications from debugging.

--- base64-thread.py
+++ base64-thread.py
@@ -1,19 +1,16 @@
 #!/bin/python
 import multiprocessing as mp
 import sys
-#print("Number of processors: ", mp.cpu_count())
-pool = mp.Pool(2)
+pool = mp.Pool(mp.cpu_count())
 
 outFile = open('output.txt','w')
 
 def readfile(passtext):
 	lineList = [line.rstrip('\n') for line in open(passtext)]
-	#print(lineList)
 	return appendString(lineList)
 
 def appendString(lineList):
 	authList = ['admin:' + p for p in lineList]
-	#print(authList)
 	return convertB64(authList)
 
 def convertB64(authList):
@@ -22,9 +19,8 @@ def convertB64(authList):
 		convertText = i.encode('base64', 'strict').rstrip('\n')
 		convertList.append(convertText)
 		print >>outFile, convertText
-		#print(convertList) 
 
 results = pool.apply(readfile(sys.argv[1]))
 
 pool.close()
-#print(results)
+
