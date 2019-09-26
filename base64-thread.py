a847fc500dbed8960d2ba590aaa925846589c49d
nate <nathankrunaldue@gmail.com>
2019-09-26 14:19:12 -0400

    Modified to notify user on completion.

--- base64-thread.py
+++ base64-thread.py
@@ -1,27 +1,38 @@
 #!/bin/python
 import multiprocessing as mp
+import traceback
 import sys
+import os
 pool = mp.Pool(mp.cpu_count())
-
-outFile = open('output.txt','w')
 
 def readfile(passtext):
 	lineList = [line.rstrip('\n') for line in open(passtext)]
-	return appendString(lineList)
+	return lineList
 
-def appendString(lineList):
-         #Need to create function to allow user to modify leading text.
-        authList = ['admin:' + p for p in lineList]
+
+def appendString(startList):
+	#Need to create function to allow user to modify leading text.
+	authList = ['admin:' + p for p in startList]
 	return convertB64(authList)
-	
+
 def convertB64(authList):
  	convertList = []
  	for i in authList:
  		convertText = i.encode('base64', 'strict').rstrip('\n')
  		#Need to create another function to allow user to specify output method stdout/file
-                 #convertList.append(convertText)
+        #convertList.append(convertText)
  		print >>outFile, convertText
 
-results = pool.apply(readfile(sys.argv[1]))
+
+if os.path.exists("output.txt"):
+	os.remove("output.txt")
+	print("Starting...")
+
+try:
+	outFile = open('output.txt','w')
+	startFile = readfile(sys.argv[1])
+	pool.apply(appendString(startFile))
+except Exception as e:
+	print('Complete!')
 
 pool.close()
