787ffb2720505af3992538ed71e39bf4f20dbb63
nate <nathankrunaldue@gmail.com>
2019-09-26 11:53:21 -0400

    Fixed list return errors in originial commit.

--- base64-thread.py
+++ base64-thread.py
@@ -10,19 +10,18 @@ def readfile(passtext):
 	return appendString(lineList)
 
 def appendString(lineList):
-        #Need to create function to allow user to modify leading text.
+         #Need to create function to allow user to modify leading text.
         authList = ['admin:' + p for p in lineList]
 	return convertB64(authList)
-
+	
 def convertB64(authList):
-	convertList = []
-	for i in authList:
-		convertText = i.encode('base64', 'strict').rstrip('\n')
-		#Need to create another function to allow user to specify output method stdout/file
-                #convertList.append(convertText)
-		print >>outFile, convertText
+ 	convertList = []
+ 	for i in authList:
+ 		convertText = i.encode('base64', 'strict').rstrip('\n')
+ 		#Need to create another function to allow user to specify output method stdout/file
+                 #convertList.append(convertText)
+ 		print >>outFile, convertText
 
 results = pool.apply(readfile(sys.argv[1]))
 
 pool.close()
-
