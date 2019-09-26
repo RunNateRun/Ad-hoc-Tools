85129fa9beb027908634bae91b05b7ab4fb74aa6
nate <nathankrunaldue@gmail.com>
2019-09-26 09:54:35 -0400

    Adding comments for future work.

--- base64-thread.py
+++ base64-thread.py
@@ -10,14 +10,16 @@ def readfile(passtext):
 	return appendString(lineList)
 
 def appendString(lineList):
-	authList = ['admin:' + p for p in lineList]
+        #Need to create function to allow user to modify leading text.
+        authList = ['admin:' + p for p in lineList]
 	return convertB64(authList)
 
 def convertB64(authList):
 	convertList = []
 	for i in authList:
 		convertText = i.encode('base64', 'strict').rstrip('\n')
-		convertList.append(convertText)
+		#Need to create another function to allow user to specify output method stdout/file
+                #convertList.append(convertText)
 		print >>outFile, convertText
 
 results = pool.apply(readfile(sys.argv[1]))
